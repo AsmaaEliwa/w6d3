3db30ca468d29d2e9fb7e1ed9e947c58e1904a64
artemplv <artemplv@gmail.com>
2023-06-07 17:17:23 -0400

    add controllers

--- demo_project/app/controllers/users_controller.rb
+++ demo_project/app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-def index
-    render plain: "hello"
-end
+    def index
+        render json: params
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
 end
