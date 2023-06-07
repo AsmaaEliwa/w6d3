f1312dc3aa5fcc730d8f30697ef72952fb5760db
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-06-07 18:01:12 -0400

    done ph5

--- demo_project/app/controllers/users_controller.rb
+++ demo_project/app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
     def index
-        render json: params
+        users=User.all
+        render json: users
     end
 
     def create
-        render json: params
+        user=User.new(params.require(:user).permit(:name,:email))
+       if user.save
+        render json: user
+       else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+       end
     end
 
     def show
-        render json: params
+        render json: User.find(params[:id])
     end
+    def update
+        user=User.find(params[:id])
+        if user.update(params.require(:user).permit(:name,:email))
+        render json: user
+        end
+    end
+
 end
