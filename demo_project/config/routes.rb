8b53cc796c2cdbba04c80c9199fc0d68f27bd3f9
artemplv <artemplv@gmail.com>
2023-06-07 16:25:46 -0400

    add users routes

--- demo_project/config/routes.rb
+++ demo_project/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users
+  # get 'users', to: 'users#index'
+  # get 'users/new', to: 'users#new'
+  # post 'users', to: 'users#create'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
+  # get 'users/:id/edit', to: 'users#edit'
 end
